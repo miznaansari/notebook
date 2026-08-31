28b976abe2ca0cc2c5d049fb3cb30c7d1f36b07d
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-08-31 22:08:54 +0530

    i

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
